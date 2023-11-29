d6c8e75241e03fed51f9c3bbce862a1a9ce6a67c
KayDiJay <99592596+KayDiJay@users.noreply.github.com>
2023-11-30 01:56:19 +0900

    복구완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: deprecated_member_use
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_application_1/screens/chat.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,12 +20,21 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'flutter demo',
-        theme: ThemeData().copyWith(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
-        ),
-        home: const AuthScreen());
+      title: 'flutter demo',
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 63, 17, 177)),
+      ),
+      home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (ctx, snapshot) {
+            if (snapshot.hasData) {
+              return const ChatScreen();
+            }
+
+            return const AuthScreen();
+          }),
+    );
   }
 }
